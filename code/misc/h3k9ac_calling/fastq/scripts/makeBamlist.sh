fb3056aa5c22ffce35f56aaa294a5c230e092c69
jackytao18 <112923338+jackytao18@users.noreply.github.com>
2022-10-04 17:45:09 -0400

    Delete code directory

--- code/misc/h3k9ac_calling/fastq/scripts/makeBamlist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-readarray -t samples < <(tail --lines +2 ../experimentDesign/sampleSheet.csv | cut -d, -f1)
-
-for s in "${samples[@]}"
-do
-   echo "/mnt/mfs/ctcn/datasets/rosmap/h3k9ac/dlpfcTissue/batch1/source/${s}.bam" >> /mnt/mfs/ctcn/datasets/rosmap/h3k9ac/dlpfcTissue/batch1/fastq/scripts/sourceBam_n726.txt
-done
